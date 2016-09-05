b0ae363df1dafafa5f042afa5af09db4db011913
Jonas A. Laursen <jona8690@edu.eal.dk>
2016-09-05 11:26:11 +0200

    Done exercise

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,17 @@
+﻿namespace _00_Hello
+{
+    public class Functions
+    {
+        public static string Hello(string Input = null)
+        {
+            string rs = "hello";
+            
+            if(Input != null)
+            {
+                rs = rs + ", " + Input;
+            }
+
+            return rs;
+        }
+    }
+}
